ea0f1c7f87cc786abd2e08bf32e3099fcee0bc5c
rturrado <rturrado@gmail.com>
2025-03-05 14:37:34 +0100

    Add empty board and console test files.

---
+++ test/board.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+#include "knecht4/board.hpp"
+
+namespace k4 {
+
+}  // namespace k4
---
+++ test/console.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+#include "knecht4/utils/console.hpp"
+
+namespace k4 {
+
+}  // namespace k4
